e65b67d380797fbedbc80421bacd1be27daade82
javion25 <228806684@qq.com>
2017-09-29 16:23:04 +0800

    update

--- server.js
+++ server.js
@@ -1,35 +1,28 @@
 const Vue = require('vue')
-const Koa = require('koa');
-const app = new Koa();
-const Router = require('koa-better-router');
-const router = Router().loadMethods();
-const renderer = require('vue-server-renderer').createRenderer({
-  template: require('fs').readFileSync('./index.template.html', 'utf-8')
-});
-const createApp = require('./app');
-
+const server = require('express')()
 
 const createApp = require('/path/to/built-server-bundle.js')
 
+const renderer = require('vue-server-renderer').createRenderer({
+  template: require('fs').readFileSync('./src/index.template.html', 'utf-8')
+})
 
-router.get('*', (ctx, next) => {
-  const context = { url: ctx.request.url }
+server.get('*', (req, res) => {
 
-
+  const context = { url: req.url }
+  
   createApp(context).then(app => {
     renderer.renderToString(app, (err, html) => {
       if (err) {
-        return ctx.body = 'Internal Server Error'
+        if (err.code === 404) {
+          res.status(404).end('Page not found')
+        } else {
+          res.status(500).end('Internal Server Error')
+        }
       } else {
-        return ctx.body = html
+        res.end(html)
       }
-    })   
+    })
   })
-
-});
-
-app.use(router.middleware());
-
-app.listen(3000, () => {
-  console.log('Listening at 3000...')
-});
+})
+server.listen(8080)
--- src/app.js
+++ src/app.js
@@ -5,19 +5,17 @@ import { createRouter } from './router'
 import { createStore } from './store'
 import { sync } from 'vuex-router-sync'
 
-// 导出一个工厂函数，用于创建新的
-// 应用程序、router 和 store 实例
-export function createApp () {
-  // 创建 router 和 store 实例
+module.exports = function createApp () {
+  // 创建 router 实例
   const router = createRouter()
   const store = createStore()
   // 同步路由状态(route state)到 store
   sync(store, router)
   // 创建应用程序实例，将 router 和 store 注入
   const app = new Vue({
+    // 根实例简单的渲染应用程序组件。
     router,
     store,
-    // 根实例简单的渲染应用程序组件。
     render: h => h(App)
   })
   return { app, router, store }
