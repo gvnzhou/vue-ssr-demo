81e2134f4f223fd065cc6578f09bd061f3c23bc3
javion25 <228806684@qq.com>
2017-09-30 14:12:46 +0800

    update

--- server.js
+++ server.js
@@ -1,28 +1,29 @@
 const Vue = require('vue')
 const server = require('express')()
 
-const createApp = require('/path/to/built-server-bundle.js')
+const serverBundle = require('/path/to/built-server-bundle.js')
 
-const renderer = require('vue-server-renderer').createRenderer({
-  template: require('fs').readFileSync('./src/index.template.html', 'utf-8')
+const { createBundleRenderer } = require('vue-server-renderer')
+const renderer = createBundleRenderer(serverBundle, {
+  runInNewContext: false, // 推荐  
+  template: require('fs').readFileSync('./src/index.template.html', 'utf-8'),
+  clientManifest // （可选）客户端构建 manifest
 })
 
 server.get('*', (req, res) => {
 
   const context = { url: req.url }
   
-  createApp(context).then(app => {
-    renderer.renderToString(app, (err, html) => {
-      if (err) {
-        if (err.code === 404) {
-          res.status(404).end('Page not found')
-        } else {
-          res.status(500).end('Internal Server Error')
-        }
+  renderer.renderToString(context, (err, html) => {
+    if (err) {
+      if (err.code === 404) {
+        res.status(404).end('Page not found')
       } else {
-        res.end(html)
+        res.status(500).end('Internal Server Error')
       }
-    })
+    } else {
+      res.end(html)
+    }
   })
 })
 server.listen(8080)
